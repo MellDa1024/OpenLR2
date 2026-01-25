MellDa1024/OpenLR2

commit 664b91eea8e54eda97464869f68ed27cadcff401

fix: UTF-8 part 5

LR2/En_xml.cpp
@@ -1,5 +1,32 @@
 ﻿#include "En_xml.h"
 #include "tinyxml/tinyxml.h"
+#include "En_fileutil.h"
+#include <fstream>
+#include <sstream>
+
+void file_utf_to_ansi(const char* filepath) {
+	std::string ansi;
+	{
+		std::ifstream fileIn(filepath);
+		std::stringstream utf;
+		utf << fileIn.rdbuf();
+		ansi = utf2ansi(utf.str(), 932);
+	}
+	{
+		std::ofstream fileOut(filepath, std::ios::trunc);
+		fileOut.write(ansi.c_str(), ansi.length());
+	}
+}
+
+bool parse_xml_utf(TiXmlDocument* xml, const char* filepath) {
+	std::ifstream file(filepath);
+	if (!file.good()) return false;
+	std::stringstream total;
+	total << file.rdbuf();
+	std::string totalUtf = ansi2utf(total.str(), 932);
+	xml->Parse(totalUtf.c_str(), 0, TIXML_ENCODING_UTF8);
+	return true;
+}
 
 int ReadXml_Int(const char *level1, const char *level2, const char *level3, int initvalue, int *oBuf, TiXmlDocument *xmlData){
 	TiXmlElement *cur;

LR2/En_xml.h
@@ -3,6 +3,9 @@
 
 #include "structure.h"
 
+void file_utf_to_ansi(const char* filepath);
+bool parse_xml_utf(TiXmlDocument* xml, const char* filepath);
+
 int ReadXml_Int(const char *level1, const char *level2, const char *level3, int initvalue, int *oBuf, TiXmlDocument *xmlData);
 int ReadXml_Str(const char *level1, const char *level2, const char *level3, const CSTR initvalue, CSTR* oBuf, TiXmlDocument *xmlData);
 int ReadXml_Int_Multi(const char * level1, const char * level2, const char * level3, int * oBuf, TiXmlDocument * xmlData);

LR2/LR2_configsave.cpp
@@ -1,6 +1,8 @@
 ﻿#include "LR2_configsave.h"
+#include "En_xml.h"
 #include "filesystem.h"
 #include <filesystem>
+#include <fstream>
 #include <iterator>
 
 static void adjust_input_filepath(CSTR& path)
@@ -58,7 +60,7 @@ int ReadKeyConfig(game *game, const char *FilePath) {
 	memset(game->config.input.buttonMap, 0, 16 * 40 * sizeof(int));
 
 	hXml = new TiXmlDocument(FilePath);
-	if (hXml->LoadFile(TIXML_ENCODING_UNKNOWN) == false) {
+	if (!parse_xml_utf(hXml, FilePath)) {
 		if (hXml) {
 			delete(hXml);
 		}
@@ -140,7 +142,7 @@ int ReadMIDI(game *gs, const char *filepath){
 	(gs->config).input.midi_control[0x27] = 0;
 
 	hXml = new TiXmlDocument(filepath);
-	if (hXml->LoadFile(TIXML_ENCODING_UNKNOWN) == false) {
+	if (!parse_xml_utf(hXml, filepath)) {
 		if (hXml) {
 			delete(hXml);
 		}
@@ -195,7 +197,7 @@ int WriteConfigXml(game *g, const char *filename){
 	
 	pFile = fopen(filename, "w");
 	if (pFile == NULL) return 0; 
-	
+
 	fputs("<?xml version=\"1.0\" encoding=\"shift_jis\"?>\n", pFile);
 	fputs("<config>\n", pFile);
 
@@ -606,6 +608,7 @@ int WriteConfigXml(game *g, const char *filename){
 
 	fputs("</config>\n", pFile);
 	fclose(pFile);
+	file_utf_to_ansi(filename);
 	return 1;
 }
 
@@ -742,6 +745,7 @@ int WriteKeyConfig(game *g, const char *filepath, int key) {
 
 	fputs("</keyconfig>\n", pFile);
 	fclose(pFile);
+	file_utf_to_ansi(filepath);
 	return 1;
 }
 
@@ -799,6 +803,7 @@ int WriteMidiXml(game *g, const char *filename) {
 	fputs("\t</program>\n", pFile);
 	fputs("</midi>\n", pFile);
 	fclose(pFile);
+	file_utf_to_ansi(filename);
 	return 1;
 }
 
@@ -841,7 +846,7 @@ int ReadSkinCustomize(SkinUser *sku, char *FilePath) {
 	(sku->adjust).rate_y = 100;
 	
 	hXml = new TiXmlDocument(FilePath);
-	if (hXml->LoadFile(TIXML_ENCODING_UNKNOWN) == false) {
+	if (!parse_xml_utf(hXml, FilePath)) {
 		if (hXml) {
 			delete(hXml);
 		}
@@ -1023,7 +1028,7 @@ int WriteSkinCustomizeXml(SkinUser *sku, char *filepath) {
 
 	fputs("</skincustomize>\n", pFile);
 	fclose(pFile);
-
+	file_utf_to_ansi(filepath);
 	return 1;
 }
 
@@ -1039,7 +1044,7 @@ int ReadConfig(game* g, const char* filepath) {
 	hXml = new TiXmlDocument(filepath);
 
 	hXml->SetCondenseWhiteSpace(false);
-	if (hXml->LoadFile(TIXML_ENCODING_UNKNOWN) == false) {
+	if (!parse_xml_utf(hXml, filepath)) {
 		if (hXml) {
 			delete(hXml);
 		}

LR2/LR2_ir.cpp
@@ -8,6 +8,7 @@
 #include "En_dbio.h"
 #include "En_fileutil.h"
 #include "En_timer.h"
+#include "En_xml.h"
 #include "filesystem.h"
 
 #ifdef _WIN32
@@ -113,16 +114,7 @@ int RANKING::ParseXML(const char* path) {
 
 	TiXmlDocument *hXml = new TiXmlDocument(path);
 
-	std::string s;
-	{
-		std::ifstream ifs{path};
-		std::stringstream ss;
-		ss << ifs.rdbuf();
-		s = ss.str();
-	}
-	s = ansi2utf(s.c_str(), 932);
-	// NOTE: TIXML_ENCODING_UTF8 or for whatever other reason it ignores `encoding='shift_jis'` in the header.
-	hXml->Parse(s.c_str(), nullptr, TIXML_ENCODING_UTF8);
+	parse_xml_utf(hXml, path);
 
 	TiXmlElement *cur;
 	cur = hXml->FirstChildElement("lastupdate");
@@ -273,13 +265,7 @@ int ParseRivalData(long ID) {
 	
 	cstrSprintf(&path, fs::make_preferred("LR2files/Rival/%d.xml").data(), ID);
 	hXml = new TiXmlDocument(path);
-	if (hXml->LoadFile(TIXML_ENCODING_UNKNOWN) == false) {
-		if (hXml) {
-			delete(hXml);
-		}
-		printfDx("ID%4d:ライバルデータにアクセスできません。\n", ID);
-		return 0;
-	}
+	parse_xml_utf(hXml, path.body);
 
 	std::string name;
 	cur = hXml->FirstChildElement("rivalname");
@@ -396,10 +382,10 @@ int ParseRivalData(long ID) {
 	SQL_Run("COMMIT", pRivalDB);
 	sqlite3_close(pRivalDB);
 
-	std::string name_as_cp932 = utf2ansi(name, 932);
 	CSTR cfolder;
 	cstrSprintf(&cfolder, "#COMMAND __RIVAL__\n#MAXTRACKS %d\n#CATEGORY ライバルフォルダ\n#TITLE %s\n#INFORMATION_A %sのプレイした曲を表示します\n#INFORMAION_B\n",
-			ID, name_as_cp932.c_str(), name_as_cp932.c_str());
+			ID, name.c_str(), name.c_str());
+	cfolder = utf2ansi(cfolder.body, 932).c_str();
 	cstrSprintf(&path, fs::make_preferred("LR2files/Rival/%d.lr2folder").data(), ID);
 	cfolder.toFile(path);
 	printfDx("ID%06d:ライバルデータ[%s]を更新しました。更新スコア数%d\n", ID, name.c_str(), count);
@@ -425,8 +411,9 @@ int NETWORK::GetInsaneList() {
 	}
 	ScreenFlip();
 
-	hXml = new TiXmlDocument(fs::make_preferred("LR2files/Database/exlevel.xml").data());
-	if (hXml->LoadFile(TIXML_ENCODING_UNKNOWN) == false) {
+	std::string path = fs::make_preferred("LR2files/Database/exlevel.xml").data();
+	hXml = new TiXmlDocument(path.c_str());
+	if (!parse_xml_utf(hXml, path.c_str())) {
 		if (hXml) {
 			delete(hXml);
 		}
@@ -793,7 +780,7 @@ int NETWORK::GetTargetInfo(int mode, CSTR songmd5, CSTR *oData, CSTR *oName, int
 	}
 
 	CSVbuf csv;
-	SplitCSV(httpResult, &csv, ",");
+	SplitCSV(ansi2utf(httpResult.body, 932).c_str(), &csv, ",");
 	if (csv.str[1].length()) {
 		if (mode == 8) {
 			*oName = "AVERAGE";

commit 270c75f8a0e4ff6ced1f01655cddeffdae47eec5

refactor: check xml->Parse error status
Fixes: 664b91eea8e5 ("fix: UTF-8 part 5")

LR2/En_xml.cpp
@@ -24,8 +24,7 @@ bool parse_cp932_xml(TiXmlDocument* xml, const char* filepath) {
 	std::stringstream total;
 	total << file.rdbuf();
 	std::string totalUtf = ansi2utf(total.str(), 932);
-	xml->Parse(totalUtf.c_str(), 0, TIXML_ENCODING_UTF8);
-	return true;
+	return xml->Parse(totalUtf.c_str(), 0, TIXML_ENCODING_UTF8) != nullptr;
 }
 
 int ReadXml_Int(const char *level1, const char *level2, const char *level3, int initvalue, int *oBuf, TiXmlDocument *xmlData){