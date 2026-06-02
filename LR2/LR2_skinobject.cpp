f0253f423c369b8136f59d5472319f8296d9e84a
GOMazk <gommazuk@naver.com>
2026-06-02 20:42:48 +0900

    feat: expand judgetiming (temp)
    skins only show 2 digits, so it's temporary

--- LR2/LR2_skinobject.cpp
+++ LR2/LR2_skinobject.cpp
@@ -3375,7 +3375,7 @@ int SetObjectValue_Button(game *g, skstruct *sk, Timer *T, char flag) {
 				break;
 
 			case 74:
-				isClickSuccess = ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->config.play.judgetiming, -99, 99, g->sSelect.panel);
+				isClickSuccess = ButtonByInput(&sk->drBuf, &sk->otherObject[1].src[i], &sk->otherObject[1].dst[i], T, &g->KeyInput, &g->config.play.judgetiming, -199, 199, g->sSelect.panel);
 				if (isClickSuccess == 2) {
 					PlaySound(&g->audio, &g->audio.sysSound.option_change, g->audio.chnKey, -1);
 					SetObjectStrings_SongSelect(g);
