MellDa1024/OpenLR2

commit 049d14081911653544301c8fd5991016f7204987

feat: utf-8 strings

LR2/Main.cpp
@@ -38,6 +38,13 @@ static std::filesystem::path GetExecutablePath()
 }
 */
 
+static std::filesystem::path GetExecutablePath()
+{
+	std::wstring exePath(MAX_PATH, 0);
+	GetModuleFileNameW(nullptr, exePath.data(), MAX_PATH);
+	return std::filesystem::path(exePath).parent_path();
+}
+
 #else
 
 #include <iostream>
@@ -96,21 +103,11 @@ int main(int argc, char** argv) {
 
 	int tmp;
 
-#ifdef _WIN32
-	{
-		char curDir[260];
-		GetModuleFileName(NULL, (LPCH)curDir, 260);
-		*(char*)(strrchr(curDir, '\\') + 1) = '\x00';
-		SetCurrentDirectory((LPCSTR)curDir);
-		gs.baseDirectory.assign(curDir, 0).add("\\");
-	}
-#else // TODO(utf-8): use this
 	{
 		auto curDir = GetExecutablePath();
 		std::filesystem::current_path(curDir);
 		gs.baseDirectory.assign(curDir.string().c_str(), 0).add("/");
 	}
-#endif // _WIN32
 
 	gs.is_starter = false;
 	auto copy_if_not_exists = [](auto&& from, auto&& to_) {
@@ -337,6 +334,8 @@ int main(int argc, char** argv) {
 		ErrorLogFmtAdd("動画作成モードなのでVSyncを待ちます。\n");
 	}
 #ifdef _WIN32
+	SetUseCharCodeFormat(DX_CHARCODEFORMAT_UTF8);
+	SetFontCharCodeFormat(DX_CHARCODEFORMAT_UTF8);
 	SetMultiThreadFlag(1);
 	SetUseFPUPreserveFlag(1);
 	SetUseDirectInputFlag(1); //DXLIBVER: not in original, but we need it to make same reaction.
@@ -390,8 +389,8 @@ int main(int argc, char** argv) {
 	}
 	if (gs.is_starter == false) {
 		if (gs.cmd_directplay == false) {
-			printfDx(LR2VERSIONSTRING);
-			printfDx("\nPUSH ANY KEY\n");
+			printfDx(LR2VERSIONSTRING"\n");
+			printfDx("PUSH ANY KEY\n");
 			if (loadingGrHandle > 0) {
 				DrawGraph(0, 0, loadingGrHandle, 0);
 			}
@@ -470,7 +469,6 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < 20; i++) gs.skstruct.customfile[i].fillzero();
 	gs.skstruct.skinMD5.fillzero();
 	gs.skstruct.skFontname.fillzero();
-	memset(&gs.skstruct, 0, sizeof(skstruct));
 	for (int i = 0; i < 200; i++) gs.skstruct.caption[i].fillzero();
 	for (int i = 0; i < 200; i++) gs.skstruct.caption[i].assign("(null)");
 	for (int i = 0; i < 200; i++) gs.skstruct.GrHandle[i] = -1;
@@ -520,7 +518,6 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < 20; i++) gs.skstruct2.customfile[i].fillzero();
 	gs.skstruct2.skinMD5.fillzero();
 	gs.skstruct2.skFontname.fillzero();
-	memset(&gs.skstruct2, 0, sizeof(skstruct));
 	for (int i = 0; i < 200; i++) gs.skstruct2.caption[i].fillzero();
 	for (int i = 0; i < 200; i++) gs.skstruct2.caption[i].assign("(null)");
 	for (int i = 0; i < 200; i++) gs.skstruct2.GrHandle[i] = -1;

LR2/strclass.cpp
@@ -6,6 +6,9 @@
 #include <string_view>
 #include <cstdarg>
 #include <cctype>
+#include <filesystem>
+
+#include "En_fileutil.h"
 
 typedef unsigned char byte;
 
@@ -50,53 +53,13 @@ DWORD CSTR::CRC32() {
 	if (0 < (int)(pcVar3 + 1)) {
 		do {
 			uVar4 = uVar4 ^ (int)pcVar2[iVar5];
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
-			}
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
-			}
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
-			}
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
-			}
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
-			}
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
-			}
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
-			}
-			if ((uVar4 & 1) == 0) {
-				uVar4 = uVar4 >> 1;
-			}
-			else {
-				uVar4 = uVar4 >> 1 ^ 0xedb88320;
+			for (int i = 0; i < 8; i++) {
+				if ((uVar4 & 1) == 0) {
+					uVar4 = uVar4 >> 1;
+				}
+				else {
+					uVar4 = uVar4 >> 1 ^ 0xedb88320;
+				}
 			}
 			iVar5 = iVar5 + 1;
 		} while (iVar5 < (int)(pcVar3 + 1));
@@ -371,7 +334,7 @@ int CSTR::toFile(const char *filepath) {
 	FILE *_File;
 	char *pcVar2;
 
-	_File = fopen(filepath, "w");
+	_File = _wfopen(utf2ws(filepath).c_str(), L"w");
 	if (_File == (FILE *)0x0) {
 		return -1;
 	}
@@ -451,7 +414,7 @@ char* CSTR::atPos(int pos) {
 bool CSTR::canOpenFile() {
 	FILE *_File;
 
-	_File = fopen(body, "r");
+	_File = _wfopen(utf2ws(body).c_str(), L"r");
 	if (_File == NULL) {
 		return false;
 	}
@@ -569,34 +532,21 @@ CSTR& CSTR::operator=(const char *str) {
 	return assign(str, 0);
 }
 
-CSTR CSTR::getDirectory() {
-	byte ch;
-	int len;
-	char *str;
-	int pos;
-	int i;
-
-	i = 0;
-	str = this->body;
-	pos = -1;
-	len = length();
-	while (str[i] != '\0') {
-		if ( i >= len - 1 ) break;
-		ch = str[i];
-		if ((byte)((ch ^ 0x20) + 0x5f) < 0x3c) {
-			i = i + 2;
-		}
-		else {
-			if ((ch == '\\') || (ch == '/')) {
-				pos = i;
-			}
-			i = i + 1;
-		}
-	}
-	return left(pos + 1);
+[[nodiscard]] inline const char* cs(const std::u8string& s)
+{
+	// Don't try making this take fs::path, pray on lifetime extension.
+	return reinterpret_cast<const char*>(s.c_str());
 }
 
-CSTR CSTR::getParaentDirectory() {
+CSTR CSTR::getDirectory() {
+	CSTR out;
+	std::filesystem::path path(reinterpret_cast<char8_t*>(this->body));
+	out.assign(cs(path.parent_path().u8string()));
+	*out.atPos(out.length()) = std::filesystem::path::preferred_separator;
+	return out;
+}
+
+CSTR CSTR::getParentDirectory() {
 	return getDirectory().getDirectory();
 }
 

LR2/En_audio.cpp
@@ -441,7 +441,7 @@ void WriteSoundFile(AUDIO *aud, CSTR filename, uint size) {
 	FILE* _File;
 	char header1[12], header2[24], header3[8];
 
-	_File = fopen(filename, "wb");
+	_File = _wfopen(utf2ws(filename.body).c_str(), L"wb");
 	if ((size != 0) && (size <= aud->size)) {
 		aud->size = size;
 	}
@@ -597,21 +597,6 @@ int RecordFadeout(AUDIO *aud, double from, double length) {
 	return 1;
 }
 
-static std::string s2utf8(const std::string_view str, unsigned int codepage) {
-#ifdef _WIN32
-	int size_needed = MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
-	auto wstr = std::make_unique_for_overwrite<wchar_t[]>(size_needed);
-	MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), wstr.get(), size_needed);
-	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
-	return converter.to_bytes(wstr.get(), wstr.get() + size_needed);
-#else
-	if (codepage == CP_ACP) { // Already UTF-8
-		return std::string{str};
-	}
-	return std::string{str}; // unused currently :P
-#endif // _WIN32
-}
-
 int LoadSound(AUDIO *aud, SOUNDDATA *sound, CSTR filepath, int loop, int /*disableDSP*/, int previewFlag) {
 
 	CSTR path;
@@ -638,7 +623,7 @@ int LoadSound(AUDIO *aud, SOUNDDATA *sound, CSTR filepath, int loop, int /*disab
 		FMOD_RESULT result;
 		if (aud->cmd_mediaOut) {
 			FMOD_MODE mode = FMOD_ACCURATETIME | FMOD_LOOP_OFF;
-			result = FMOD_System_CreateSound(aud->fmodSys, filepath, mode, NULL, &sound->fmod_sound); 
+			result = FMOD_System_CreateSound(aud->fmodSys, filepath.body, mode, nullptr, &sound->fmod_sound); 
 			SOUND_normalize(aud, sound);
 		}
 		else {
@@ -646,7 +631,7 @@ int LoadSound(AUDIO *aud, SOUNDDATA *sound, CSTR filepath, int loop, int /*disab
 			if (previewFlag != 0) {
 				mode |= FMOD_CREATESTREAM;
 			}
-			result = FMOD_System_CreateSound(aud->fmodSys, s2utf8(filepath.body, CP_ACP).c_str(), mode, NULL, &sound->fmod_sound);
+			result = FMOD_System_CreateSound(aud->fmodSys, filepath.body, mode, nullptr, &sound->fmod_sound);
 		}
 
 		sound->flag2c = 0;
@@ -668,7 +653,7 @@ int LoadSound(AUDIO *aud, SOUNDDATA *sound, CSTR filepath, int loop, int /*disab
 	}
 
 	if (previewFlag) SetCreateSoundDataType(3);
-	sound->soundHandle = LoadSoundMem(filepath, 3, -1);
+	sound->soundHandle = LoadSoundMem(filepath.body, 3, -1);
 	if (previewFlag) SetCreateSoundDataType(0);
 	if (sound->soundHandle == -1) {
 		sound->filename.fillzero();

LR2/En_dbio.cpp
@@ -60,23 +60,11 @@ static bool UTF8toANSI(LPCSTR str, char *oBuf, size_t *oSize){
 }
 
 int SQL_Run(CSTR queryStr, sqlite3 *sql) {
-	size_t size;
-	ANSItoUTF8(queryStr, nullptr, &size);
-	CSTR oBuf;
-	oBuf.resize2(size + 1);
-	memset(oBuf.body, 0, size + 1);
-	ANSItoUTF8(queryStr, oBuf.body, &size);
-	return sqlite3_exec(sql, oBuf.body, nullptr, nullptr, nullptr);
+	return sqlite3_exec(sql, queryStr.body, nullptr, nullptr, nullptr);
 }
 
 int SQL_prepare(CSTR queryStr, sqlite3 *sql, sqlite3_stmt **ppStmt) {
-	size_t size;
-	ANSItoUTF8(queryStr, nullptr, &size);
-	CSTR oBuf;
-	oBuf.resize2(size + 1);
-	memset(oBuf.body, 0, size + 1);
-	ANSItoUTF8(queryStr, oBuf.body, &size);
-	return sqlite3_prepare(sql, oBuf.body, -1, ppStmt, nullptr);
+	return sqlite3_prepare(sql, queryStr.body, -1, ppStmt, nullptr);
 }
 
 CSTR SQL_GetColumn(int i, sqlite3_stmt *pStmt){
@@ -89,15 +77,6 @@ CSTR SQL_GetColumn(int i, sqlite3_stmt *pStmt){
 	// Cast safety: it's safe to cast from unsigned char* to char*
 	const char* columnText = reinterpret_cast<const char*>(sqlite3_column_text(pStmt, i));
 
-	size_t size;
-	UTF8toANSI(columnText, nullptr, &size);
-
-	CSTR oBuf;
-	oBuf.resize2(size + 1);
-	memset(oBuf.body, 0, size + 1);
-
-	UTF8toANSI(columnText, oBuf.body, &size);
-	oBuf.body[size] = '\0';
-
+	CSTR oBuf(columnText);
 	return oBuf;
 }

LR2/En_fileutil.cpp
@@ -1,6 +1,7 @@
 ﻿#include "En_fileutil.h"
 #include <md5.h>
 #include "DxLib/DxLib.h" //log
+#include <codecvt>
 
 #ifndef _WIN32
 #include <chrono>
@@ -8,9 +9,65 @@
 #include <sys/stat.h>
 #endif // _WIN32
 
+std::wstring utf2ws(const std::string_view str) {
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, str.data(), str.size(), nullptr, 0);
+	std::wstring wstr(size_needed, 0);
+	MultiByteToWideChar(CP_UTF8, 0, str.data(), str.size(), wstr.data(), size_needed);
+	return wstr;
+}
+
+std::string ws2utf(const std::wstring_view wstr) {
+	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+	return converter.to_bytes(wstr.data());
+}
+
+std::string utf2ansi(const std::string_view in, unsigned int codepage) {
+	int size_needed = MultiByteToWideChar(CP_UTF8, 0, in.data(), in.size(), nullptr, 0);
+	std::wstring wstr(size_needed, 0);
+	MultiByteToWideChar(CP_UTF8, 0, in.data(), in.size(), wstr.data(), size_needed);
+	size_needed = WideCharToMultiByte(codepage, 0, wstr.data(), wstr.size(), nullptr, 0, 0, 0);
+	std::string out(size_needed, 0);
+	WideCharToMultiByte(codepage, 0, wstr.data(), wstr.size(), out.data(), out.size(), 0, 0);
+	return out;
+}
+
+std::string ansi2utf(const std::string_view str, unsigned int codepage) {
+	int size_needed = MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), nullptr, 0);
+	auto wstr = std::make_unique_for_overwrite<wchar_t[]>(size_needed);
+	MultiByteToWideChar(codepage, 0, str.data(), static_cast<int>(str.size()), wstr.get(), size_needed);
+	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> converter;
+	return converter.to_bytes(wstr.get(), wstr.get() + size_needed);
+}
+
+std::u32string utf8_to_utf32(const std::string& str) {
+	std::u32string out;
+	// FIXME: std::use_facet is already deprecated and removed in C++26, and is not supported well in Wine.
+	static const auto locale = std::locale("ja_JP.UTF8");
+	static const auto& facet_u32_u8 = std::use_facet<std::codecvt<char32_t, char, std::mbstate_t>>(locale);
+	out.resize(str.size() * facet_u32_u8.max_length(), '\0');
+
+	std::mbstate_t s;
+	const char* from_next = str.data();
+	char32_t* to_next = out.data();
+
+	std::codecvt_base::result res;
+	do
+	{
+		res = facet_u32_u8.in(s, from_next, &str[str.size()], from_next, to_next, &out[out.size()], to_next);
+
+		// skip unconvertiable chars (which is impossible though)
+		if (res == std::codecvt_base::error)
+			from_next++;
+
+	} while (res == std::codecvt_base::error);
+
+	out.resize(to_next - &out[0]);
+	return out;
+}
+
 int makeFileHash(LPCSTR filepath, LPCSTR oBuf) {
 	FILE* pFile;
-	pFile = fopen(filepath, "rb");
+	pFile = _wfopen(utf2ws(filepath).c_str(), L"rb");
 	if (!pFile)  return -1;
 	unsigned char* md5buf = (unsigned char*)md5File(pFile);
 	fclose(pFile);
@@ -53,9 +110,8 @@ time_t GetFileUnixtime(CSTR str) {
 	}
 
 #ifdef _WIN32
-	WIN32_FIND_DATA FindFileData;
-	LPWIN32_FIND_DATAA lpFindFileData = (LPWIN32_FIND_DATAA)&FindFileData;
-	HANDLE hFindFile = FindFirstFileA(str, lpFindFileData);
+	WIN32_FIND_DATAW FindFileData;
+	HANDLE hFindFile = FindFirstFileW(utf2ws(str.body).c_str(), &FindFileData);
 	if (hFindFile == (HANDLE)-1) {
 		ErrorLogFmtAdd("ファイルのLR2TIME取得エラー:%sが見つからない\n", str.body);
 		return -1;
@@ -80,14 +136,15 @@ CSTR GetRandomFileOnDir(CSTR path, char fOnlyName) {
 #ifdef _WIN32
 	CSTR oBuf;
 	//CSTR str1,str2,str3;
-	WIN32_FIND_DATA FindFileData;
+	WIN32_FIND_DATAW FindFileData;
 	HANDLE hFindFile;
 	int fileCount = 0;
 	CSTR str1( path.left(path.findStrPos("*")) );
 	CSTR str2( path.right(path.length() - str1.length() - 1) );
 	CSTR str3( str1 );
 	str3.add("*");
-	hFindFile = FindFirstFileA(str3, (LPWIN32_FIND_DATAA)&FindFileData);
+	std::wstring wstr = utf2ws(str3.body);
+	hFindFile = FindFirstFileW(wstr.c_str(), &FindFileData);
 	if (hFindFile == (HANDLE)-1) {
 		//oBuf = CSTR("ERROR");
 		return CSTR("ERROR");
@@ -96,37 +153,39 @@ CSTR GetRandomFileOnDir(CSTR path, char fOnlyName) {
 		if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
 			if (strcmp("..", (char*)FindFileData.cFileName) && strcmp(".", (char*)FindFileData.cFileName)) fileCount++;
 		}
-	} while (FindNextFileA(hFindFile, (LPWIN32_FIND_DATAA)&FindFileData));
+	} while (FindNextFileW(hFindFile, &FindFileData));
 	FindClose(hFindFile);
 	if (fileCount > 0) {
 		fileCount = GetRand(fileCount - 1);
 
-		hFindFile = FindFirstFileA(str3, (LPWIN32_FIND_DATAA)&FindFileData);
+		hFindFile = FindFirstFileW(wstr.c_str(), &FindFileData);
 		if (hFindFile != (HANDLE)-1) {
 			do {
 				if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-					if (strcmp("..", (char*)FindFileData.cFileName) && strcmp(".", (char*)FindFileData.cFileName)) {
+					std::string filename = ws2utf(FindFileData.cFileName);
+					if (strcmp("..", filename.c_str()) && strcmp(".", filename.c_str())) {
 						//LAB_00438327
 						int i = 0;
 						while (i < fileCount) {
-							FindNextFileA(hFindFile, (LPWIN32_FIND_DATAA)&FindFileData);
+							FindNextFileW(hFindFile, &FindFileData);
 							if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-								if (strcmp("..", (char*)FindFileData.cFileName) && strcmp(".", (char*)FindFileData.cFileName)) i++;
+								filename = ws2utf(FindFileData.cFileName);
+								if (strcmp("..", filename.c_str()) && strcmp(".", filename.c_str())) i++;
 							}
 						}
 						FindClose(hFindFile);
 						path.assign(&str1);
-						path.add((char*)FindFileData.cFileName);
+						path.add(filename.c_str());
 						path.add(&str2);
 						if (fOnlyName) {
 							//oBuf = CSTR(FindFileData.cFileName);
-							return CSTR((char*)FindFileData.cFileName);
+							return CSTR(filename.c_str());
 						}
 						//oBuf = CSTR(path);
 						return CSTR(path);
 					}
 				}
-				FindNextFileA(hFindFile, (LPWIN32_FIND_DATAA)&FindFileData);
+				FindNextFileW(hFindFile, &FindFileData);
 			} while (true);
 		}
 	}
@@ -298,10 +357,10 @@ bool IsFileExist(CSTR path) {
 
 #ifdef _WIN32
 	HANDLE hFindFile;
-	_WIN32_FIND_DATAA findFileData;
+	_WIN32_FIND_DATAW findFileData;
 	char dirFlag = 0;
 
-	hFindFile = FindFirstFileA(path, &findFileData);
+	hFindFile = FindFirstFileW(utf2ws(path.body).c_str(), &findFileData);
 	FindClose(hFindFile);
 	return hFindFile != (HANDLE)-1;
 #else
@@ -699,31 +758,33 @@ CSTR GetRandomFile(CSTR path, char fOnlyName) {
 	}
 
 #ifdef _WIN32
-	WIN32_FIND_DATA FindFileData;
+	std::wstring wpath = utf2ws(path.body);
+	WIN32_FIND_DATAW FindFileData;
 	//count files for random
-	HANDLE hFindFile = FindFirstFileA(path, (LPWIN32_FIND_DATAA)&FindFileData);
+	HANDLE hFindFile = FindFirstFileW(wpath.c_str(), &FindFileData);
 	if (hFindFile == (HANDLE)-1) return CSTR("ERROR");
 	
 	count = 0;
 	do {
 		count++;
-	} while (FindNextFileA(hFindFile, (LPWIN32_FIND_DATAA)&FindFileData));
+	} while (FindNextFileW(hFindFile, &FindFileData));
 	if (count < 1) return CSTR("ERROR");
 
 	count = GetRand(count - 1);
 
 	//get file by random
-	hFindFile = FindFirstFileA(path, (LPWIN32_FIND_DATAA)&FindFileData);
+	hFindFile = FindFirstFileW(wpath.c_str(), &FindFileData);
 	if (hFindFile == (HANDLE)-1) return CSTR("ERROR");
 
 	for (int i = 0; i < count; i++) {
-		FindNextFile(hFindFile, &FindFileData);
+		FindNextFileW(hFindFile, &FindFileData);
 	}
 	FindClose(hFindFile);
 	path.assign(path.getDirectory());
-	path.add((char*)FindFileData.cFileName);
+	std::string filename = ws2utf(FindFileData.cFileName);
+	path.add(filename.c_str());
 	if (fOnlyName) {
-		path.assign((char*)FindFileData.cFileName);
+		path.assign(filename.c_str());
 		path.nullAtPos(path.findStrPos("."));
 	}
 	return path;

LR2/En_fileutil.h
@@ -72,3 +72,10 @@ CSTR GetRandomFileNoError(CSTR path, CSTR dir);
 //md5
 void MD5byte(char **iStr, uint len, char *oByte);
 char* MD5str(char *iStr);
+
+//string convert
+std::wstring utf2ws(const std::string_view str);
+std::string ws2utf(const std::wstring_view wstr);
+std::string utf2ansi(const std::string_view in, unsigned int codepage);
+std::string ansi2utf(const std::string_view str, unsigned int codepage);
+std::u32string utf8_to_utf32(const std::string& str);

LR2/En_recordmovie.cpp
@@ -4,6 +4,7 @@
 
 #include "structure.h"
 #include "filesystem.h"
+#include "En_fileutil.h"
 
 #include <DxLib/DxLib.h>
 
@@ -381,13 +382,13 @@ int Mp3toWavF(FILE *iFile, FILE *oFile) { //TODO : need test
 bool Mp3toWavP(char *iPath, char *oPath) {
 	FILE *iFile, *oFile;
 
-	iFile = fopen(iPath, "rb");
+	iFile = _wfopen(utf2ws(iPath).c_str(), L"rb");
 	if (iFile == NULL) {
 		ErrorLogFmtAdd("入力ファイルが開けません(%s)。\n", iPath);
 		_exit(1);
 	}
 
-	oFile = fopen(oPath, "wb");
+	oFile = _wfopen(utf2ws(iPath).c_str(), L"wb");
 	if (oFile == NULL) {
 		ErrorLogFmtAdd("出力ファイルが開けません(%s)。\n", oPath);
 		_exit(1);

LR2/LR2_audio.cpp
@@ -345,7 +345,7 @@ int ReadLR2SoundSet(game *g, CSTR filepath, char reFlag) {
 		int t = sku.customize_value[i];
 		if (899 < t &&  t < 1000) dst_op[t] = 1;
 	}
-	hFile = fopen(filepath, "r");
+	hFile = _wfopen(utf2ws(filepath.body).c_str(), L"r");
 	if (!hFile) return 0;
 	CSTR fBuf(260);
 	char* pFbuf;

commit c4713ccdcae24b774cebbc3834475acccbfc898c

fix: fix getParentDirectory being the same as getDirectory
And add a rudimentary test setup. We don't need anything fancy yet.

Fixes songs being nested in their folders after a rescan.

Fixes: 049d14081911 ("feat: utf-8 strings")

LR2/Main.cpp
@@ -71,6 +71,18 @@ static consteval bool is_linux()
 #endif
 }
 
+static bool run_tests() {
+	if (CSTR fp = "C:\\a\\b\\c\\d.bms"; fp.getDirectory().body != std::string_view{"C:\\a\\b\\c\\"}) {
+		ErrorLogFmtAdd("1: %s\n", fp.getDirectory().body);
+		return false;
+	}
+	if (CSTR fp = "C:\\a\\b\\c\\d.bms"; fp.getParentDirectory().body != std::string_view{"C:\\a\\b\\"}) {
+		ErrorLogFmtAdd("2: %s\n", fp.getParentDirectory().body);
+		return false;
+	}
+	return true;
+}
+
 int main(int argc, char** argv) {
 #ifdef _DEBUG
 	while (!IsDebuggerPresent()) std::this_thread::sleep_for(std::chrono::milliseconds(200));
@@ -156,6 +168,7 @@ int main(int argc, char** argv) {
 	gs.rec.recMode = 0;
 	gs.audio.replay2avi = false;
 	gs.skstruct.drBuf.isDisabled = '\0';
+	bool test_mode = false;
 	bool use_dx9 = false;
 	//commandline
 	for (int i = 1; i < argc; i++) {
@@ -223,19 +236,28 @@ int main(int argc, char** argv) {
 		else if (tStr2.starts_with("-dx9")) {
 			use_dx9 = true;
 		}
+		else if (tStr2.starts_with("-test")) {
+			test_mode = true;
+		}
 	}
 	gs.config.system.thread = 0;
+	if (test_mode) {
+		if(!run_tests()) {
+			ErrorLogAdd("tests failed\n");
+			return 1;
+		}
+		ErrorLogAdd("tests passed\n");
+		return 0;
+	}
 	CSTR pathScoreDB;
 	cstrSprintf(&pathScoreDB, "LR2files/Database/Score/%s.db", gs.config.player.id.body);
-	if (gs.is_starter == '\0') {
-		if (IsFileExist(pathScoreDB) == false) {
+	if (!gs.is_starter) {
+		if (!IsFileExist(pathScoreDB)) {
 			MessageBoxA(NULL, "スコアデータベースが見つかりません。\nconfig.exeで作成して下さい。", "エラー", 0);
 			return -1;
 		}
-		if (gs.is_starter == '\0') {
-			if (ReadPlayerScore(gs.config.player.id, gs.config.player.pass, &gs.gameplay.playerstat) == 0) {
-				return -1;
-			}
+		if (ReadPlayerScore(gs.config.player.id, gs.config.player.pass, &gs.gameplay.playerstat) == 0) {
+			return -1;
 		}
 	}
 	//make beta3 score backup

LR2/strclass.cpp
@@ -30,14 +30,11 @@ CSTR::CSTR(int size) {
 }
 
 CSTR::~CSTR() {
-	if (body) free(body);
+	free(body);
 }
 
 int CSTR::length() {
-	if (body)
-		return strlen(body);
-	else
-		return 0;
+	return body ? strlen(body) : 0;
 }
 
 DWORD CSTR::CRC32() {
@@ -528,7 +525,11 @@ CSTR CSTR::getDirectory() {
 }
 
 CSTR CSTR::getParentDirectory() {
-	return getDirectory().getDirectory();
+	CSTR out;
+	out.assign(std::filesystem::path{ this->body }.parent_path().parent_path().string().c_str());
+	*out.atPos(out.length() + 1) = '\0';
+	*out.atPos(out.length()) = std::filesystem::path::preferred_separator;
+	return out;
 }
 
 CSTR& CSTR::cutDirectorySeparator() {