4ff87fd97bbe5aaeffb305a990a28f30c6f027f8
Skalim <gold_scorpion2007@hotmail.com>
2026-06-24 19:09:08 -0400

    fix: correct borderless mouse coordinates on multi-monitor setups

--- LR2/En_input.cpp
+++ LR2/En_input.cpp
@@ -1,5 +1,6 @@
 ﻿#include "En_input.h"
 #include "En_timer.h"
+#include "En_graphic.h"
 
 #include <DxLib/DxLib.h>
 
@@ -32,6 +33,31 @@ static MIDI midi;
 #define LOWORD(l) ((WORD)(((DWORD_PTR)(l)) & 0xffff))
 #define HIWORD(l) ((WORD)((((DWORD_PTR)(l)) >> 16) & 0xffff))
 #endif
+
+#ifdef _WIN32
+static int ScaleClientPointToSkin(int point, int clientSize, int skinSize) {
+	if (clientSize <= 0 || skinSize <= 0) return point;
+	if (point >= 0) return point * skinSize / clientSize;
+	return -((-point * skinSize + clientSize - 1) / clientSize);
+}
+
+static void FixMousePointWithClientPosition(int* mouseX, int* mouseY) {
+	const HWND hWnd = GetMainWindowHandle();
+	if (hWnd == nullptr) return;
+
+	RECT clientRect{};
+	if (GetClientRect(hWnd, &clientRect) == 0) return;
+	const int clientWidth = clientRect.right - clientRect.left;
+	const int clientHeight = clientRect.bottom - clientRect.top;
+
+	POINT cursorPoint{};
+	if (GetCursorPos(&cursorPoint) == 0) return;
+	if (ScreenToClient(hWnd, &cursorPoint) == 0) return;
+
+	*mouseX = ScaleClientPointToSkin(cursorPoint.x, clientWidth, skinSizeX);
+	*mouseY = ScaleClientPointToSkin(cursorPoint.y, clientHeight, skinSizeY);
+}
+#endif // _WIN32
 
 // TODO structure array rework
 int InitInputStructure2(inputStructure *is){
@@ -151,6 +177,9 @@ static void ProcessInput(inputStructure *is, int interval) {
 	if (GetWindowActiveFlag() == 0) return;
 
 	GetMousePoint(&mouseX, &mouseY);
+#ifdef _WIN32
+	FixMousePointWithClientPosition(&mouseX, &mouseY);
+#endif // _WIN32
 	is->mouse_moveX = mouseX - is->mouse_oldX;
 	is->mouse_moveY = mouseY - is->mouse_oldY;
 	is->mouse_moveflag = 0;
